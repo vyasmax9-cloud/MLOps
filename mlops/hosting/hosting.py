02dd29f9627c1364b5bb8a1d670263cb3fe082ff
vyasmax9-cloud <vyasmax9@gmail.com>
2026-04-28 21:15:54 +0530

    Update hosting.py
    Signed-off-by: vyasmax9-cloud <vyasmax9@gmail.com>

--- mlops/hosting/hosting.py
+++ mlops/hosting/hosting.py
@@ -4,6 +4,6 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_prediction/deployment",     # the local folder containing your files
-    repo_id="vyasmax9/tourism-prediction",          # the target repo
+    repo_id="vyasmax9/tourism-app",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
